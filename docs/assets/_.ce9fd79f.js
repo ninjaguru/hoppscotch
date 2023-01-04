af0ae2f0101d78d845995e0d2da10229de5b2e0d
ninjaguru <ninjaguru@outlook.com>
2023-01-04 11:43:56 +0530

    Add files via upload

---
+++ docs/assets/_.ce9fd79f.js
@@ -0,0 +1,2 @@
+import"./index.70b9351f.js";import{iC as o}from"./index.70b9351f.js";export{o as default};
+//# sourceMappingURL=_.ce9fd79f.js.map
---
+++ docs/assets/files.42beb886.js
@@ -0,0 +1,2 @@
+import{t as a}from"./TaskOption.065f32ae.js";const n=e=>a(()=>new Promise((o,t)=>{const r=new FileReader;r.onload=()=>{o(r.result)},r.onerror=()=>{t(new Error("File err"))},r.readAsText(e)}));export{n as r};
+//# sourceMappingURL=files.42beb886.js.map
