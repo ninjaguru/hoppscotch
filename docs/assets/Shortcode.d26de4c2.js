b6aea637cc6f655b6a923442801a9052bb19f8e9
ninjaguru <ninjaguru@outlook.com>
2023-01-11 19:40:30 +0530

    Delete docs directory

--- docs/assets/Shortcode.d26de4c2.js
+++
@@ -1,2 +0,0 @@
-import{e6 as t,eu as o,ev as r}from"./index.70b9351f.js";const s=e=>t(o,{request:JSON.stringify(e)}),a=e=>t(r,{code:e});export{s as c,a as d};
-//# sourceMappingURL=Shortcode.d26de4c2.js.map
--- docs/assets/_.ce9fd79f.js
+++
@@ -1,2 +0,0 @@
-import"./index.70b9351f.js";import{iC as o}from"./index.70b9351f.js";export{o as default};
-//# sourceMappingURL=_.ce9fd79f.js.map
