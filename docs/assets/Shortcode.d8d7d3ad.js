4416ab5d1ce7435e10c92b9c61206ef5bc9ed36c
ninjaguru <ninjaguru@outlook.com>
2023-01-11 19:47:40 +0530

    Add files via upload

---
+++ docs/assets/Shortcode.d8d7d3ad.js
@@ -0,0 +1,2 @@
+import{e6 as t,eu as o,ev as r}from"./index.4c90dc19.js";const s=e=>t(o,{request:JSON.stringify(e)}),a=e=>t(r,{code:e});export{s as c,a as d};
+//# sourceMappingURL=Shortcode.d8d7d3ad.js.map
